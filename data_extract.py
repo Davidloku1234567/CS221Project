d894eb566b1cdf398d4a6538ec0fe534e2c3d024
Jeffrey Sun <telljsun@gmail.com>
2019-10-20 16:53:40 -0700

    using Earth Engine fire burn data

--- data_extract.py
+++ data_extract.py
@@ -23,42 +23,58 @@ print(tf.__version__)
 
 
 # Use these bands for prediction.
-bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7']
-# Use Landsat 8 surface reflectance data.
-l8sr = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
+bands = ['BurnDate', 'Uncertainty', 'FirstDay', 'LastDay']
+# Burned area data from https://developers.google.com/earth-engine/datasets/catalog/MODIS_006_MCD64A1
+burned = ee.ImageCollection('MODIS/006/MCD64A1')
 
-# Cloud masking function.
-def maskL8sr(image):
-  cloudShadowBitMask = ee.Number(2).pow(3).int()
-  cloudsBitMask = ee.Number(2).pow(5).int()
-  qa = image.select('pixel_qa')
-  mask = qa.bitwiseAnd(cloudShadowBitMask).eq(0).And(
-    qa.bitwiseAnd(cloudsBitMask).eq(0))
-  return image.updateMask(mask).select(bands).divide(10000)
-
-# The image input data is a 2018 cloud-masked median composite.
-image = l8sr.filterDate('2018-01-01', '2018-12-31').map(maskL8sr).median()
+# # Cloud masking function.
+# def maskL8sr(image):
+#   cloudShadowBitMask = ee.Number(2).pow(3).int()
+#   cloudsBitMask = ee.Number(2).pow(5).int()
+#   qa = image.select('pixel_qa')
+#   mask = qa.bitwiseAnd(cloudShadowBitMask).eq(0).And(
+#     qa.bitwiseAnd(cloudsBitMask).eq(0))
+#   return image.updateMask(mask).select(bands).divide(10000)
+#
+# # The image input data is a 2018 cloud-masked median composite.
+image = burned.filterDate('2018-01-01', '2018-12-31').median()
+# image = l8sr.filterDate('2018-01-01', '2018-12-31').map(maskL8sr).median()
 
 # Use folium to visualize the imagery.
-mapid = image.getMapId({'bands': ['B4', 'B3', 'B2'], 'min': 0, 'max': 0.3})
-map = folium.Map(location=[38., -122.5])
-folium.TileLayer(
-    tiles=EE_TILES.format(**mapid),
-    attr='Google Earth Engine',
-    overlay=True,
-    name='median composite',
-  ).add_to(map)
-map.add_child(folium.LayerControl())
-map
+# mapid = image.getMapId({'bands': ['B4', 'B3', 'B2'], 'min': 0, 'max': 0.3})
+# map = folium.Map(location=[38., -122.5])
+# folium.TileLayer(
+#     tiles=EE_TILES.format(**mapid),
+#     attr='Google Earth Engine',
+#     overlay=True,
+#     name='median composite',
+#   ).add_to(map)
+# map.add_child(folium.LayerControl())
+# map
 
+# print('IMAGE', image)
 
 # Change the following two lines to use your own training data.
-labels = ee.FeatureCollection('projects/google/demo_landcover_labels')
-label = 'landcover'
+# labels = ee.FeatureCollection('projects/google/demo_landcover_labels')
+region = ee.Geometry.Rectangle(-119.224, 34.669, -99.536, 50.064)
+labels = ee.FeatureCollection.randomPoints(region)
+# labels = ee.FeatureCollection(ee.Geometry.Point(16.37, 48.225));
+label = 'BurnDate'
+
+# print('LABELS', labels)
 
 # Sample the image at the points and add a random column.
-sample = image.sampleRegions(
-  collection=labels, properties=[label], scale=30).randomColumn()
+sample = image.sampleRegions(collection=labels, properties=[label], scale=30).randomColumn()
+# sample = image.sampleRegions(scale=30).randomColumn()
+
+# print('SAMPLE', sample)
+# sample = image.stratifiedSample(
+#   numPoints= 100,
+#   classBand= 'BurnDate',
+#   region= ee.Geometry.Polygon([[[-73, 42],[-73, 41],[-72, 41],[-72, 42]]]),
+#   scale= 1000,
+#   geometries= True,
+# )
 
 # Partition the sample approximately 70-30.
 training = sample.filter(ee.Filter.lt('random', 0.7))
@@ -89,7 +105,7 @@ featureNames.append(label)
 # Create the tasks.
 trainingTask = ee.batch.Export.table.toDrive(
   collection=training,
-  description='vectorsToDriveExample',
+  description='burnExample',
   fileFormat='CSV'
 )
 
